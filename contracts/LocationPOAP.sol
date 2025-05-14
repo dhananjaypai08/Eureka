786d2d5b2a318a5bcbbfe15eafc3387ba44420c9
dhananjaypai08 <dhananjay2002pai@gmail.com>
2025-05-15 03:59:04 +0530

    mainnet contract

--- contracts/LocationPOAP.sol
+++ contracts/LocationPOAP.sol
@@ -7,62 +7,100 @@ import "@openzeppelin/contracts@4.7.0/access/Ownable.sol";
 import "@openzeppelin/contracts@4.7.0/utils/Counters.sol"; 
 
 contract LocationPOAP is ERC721, ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdCounter;
+
     struct Location {
         string title;
         string ipfs;
         string latitude;
         string longitude;
     }
-    struct PoapCount{
+
+    struct PoapCount {
         address user;
         uint256 poaps;
     }
-    mapping (address => Location[]) public user_poaps;
-    mapping(address => uint256) public user_poaps_count;
+
+    uint256 public constant MAX_MINTS_PER_USER = 9;
+
+    mapping(address => Location[]) public user_poaps;
+    mapping(address => bool) public isUser;
     address[] public users;
 
-    constructor() ERC721("LPOAP", "LPOAP") {}
-    
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIdCounter;
+    mapping(address => bool) public isWhitelisted;
+
+    event POAPMinted(address indexed user, uint256 tokenId);
+    event UserWhitelisted(address indexed user);
+
+    constructor() ERC721("LPOAP", "LPOAP") {
+        whitelist(msg.sender);
+    }
+
+    function whitelist(address user) public onlyOwner {
+        isWhitelisted[user] = true;
+        emit UserWhitelisted(user);
+    }
 
     function safeMint(address to, string memory ipfs, string memory latitude, string memory longitude, string memory title) public {
+        require(isWhitelisted[msg.sender], "Not whitelisted");
+        require(user_poaps[to].length < MAX_MINTS_PER_USER, "Max POAPs claimed");
+
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
+
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, ipfs);
-        user_poaps[to].push(Location({title : title , ipfs: ipfs , latitude: latitude, longitude: longitude}));
-        user_poaps_count[to] += 1;
-        bool exists = false;
-        for(uint i = 0; i < users.length; i++){
-            if(users[i] == to){
-                exists = true;
-                break;
-            }
-        }
-        if(!exists){
+
+        user_poaps[to].push(Location(title, ipfs, latitude, longitude));
+
+        if (!isUser[to]) {
             users.push(to);
+            isUser[to] = true;
         }
+
+        emit POAPMinted(to, tokenId);
     }
 
-    function get_all_user_poaps_count() public view returns(PoapCount[] memory){
-        PoapCount[] memory all_user_poaps = new PoapCount[](users.length); 
-        for (uint i = 0; i < users.length; i++){
-            uint poap_count = user_poaps_count[users[i]];
-             all_user_poaps[i].user = users[i];
-             all_user_poaps[i].poaps = poap_count;
+    function get_all_user_poaps_count() public view returns (PoapCount[] memory) {
+        PoapCount[] memory result = new PoapCount[](users.length);
+        for (uint i = 0; i < users.length; i++) {
+            result[i] = PoapCount(users[i], user_poaps[users[i]].length);
         }
-        return all_user_poaps;
+        return result;
     }
 
-    function get_user_poaps(address user) public view returns(Location[] memory) {
+    function get_all_users() public view returns(address[] memory){
+        return users;
+    }
+
+    function get_all_users_count() public view returns(uint256) {
+        return users.length;
+    }
+
+    function get_user_poaps(address user) public view returns (Location[] memory) {
         return user_poaps[user];
     }
 
+    // Block transfers and approvals
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal override {
+        require(from == address(0), "Token is non-transferable");
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function approve(address, uint256) public pure override {
+        revert("Approvals not allowed");
+    }
+
+    function setApprovalForAll(address, bool) public pure override {
+        revert("Approvals not allowed");
+    }
+
+    // Override burn and URI
     function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
         super._burn(tokenId);
     }
- 
+
     function tokenURI(uint256 tokenId)
         public
         view
@@ -71,14 +109,4 @@ contract LocationPOAP is ERC721, ERC721URIStorage, Ownable {
     {
         return super.tokenURI(tokenId);
     }
-
-    function _beforeTokenTransfer(
-    address from, 
-    address to, 
-    uint256 tokenId
-    ) internal override virtual {
-        require(from == address(0), "Err: token transfer is BLOCKED");   
-        super._beforeTokenTransfer(from, to, tokenId);  
-    }
-
 }
